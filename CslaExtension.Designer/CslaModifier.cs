993d48d808437d14349d206854bb203435e360ab
jbudimir_cp <12183887+jbudimir@users.noreply.github.com>
2010-07-01 19:42:39 +0000

    folder restructure

---
+++ CslaExtension.Designer/CslaModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CslaExtension
+{
+	public enum CslaModifier
+	{
+        None,
+		Default,
+		Private,
+		Public,
+		Protected,
+		Internal
+	}
+}
