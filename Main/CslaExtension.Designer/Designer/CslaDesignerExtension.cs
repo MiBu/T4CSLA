bb9fc2bc94558699f52a956276ab9792bd2079b9
jbudimir_cp <SND\jbudimir_cp>
2010-07-12 13:24:44 +0000

    Merge DEV->MAIN

---
+++ Main/CslaExtension.Designer/Designer/CslaDesignerExtension.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.ComponentModel;
+using Microsoft.Data.Entity.Design.Extensibility;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CslaExtension.Designer
+{
+	class CslaDesignerExtension : CslaExtensionBase
+	{
+
+		public CslaDesignerExtension(XElement parent, PropertyExtensionContext context)
+		{
+			_context = context;
+			_parent = parent;
+		}
+
+
+		[DisplayName("Csla BL Namespace")]
+		[Description("Business Layer Namespace (namespace of CSLA generated classes).")]
+		[Category(Constants._category)]
+		public string CslaNamespace
+		{
+			get { return getValue(Constants._xnCslaNamespace); }
+			set { setValue(Constants._xnCslaNamespace, value); }
+		}
+
+        [DisplayName("Csla DAL Namespace")]
+        [Description("Data Access Layer Namespace (namespace of generated entity classes).")]
+        [Category(Constants._category)]
+        public string CslaEntitiesNamespace
+        {
+            get { return getValue(Constants._xnCslaEntitiesNamespace); }
+            set { setValue(Constants._xnCslaEntitiesNamespace, value); }
+        }
+        
+        [DisplayName("Csla Use Nullable Types")]
+		[Description("Use nullable types in generated classes.")]
+		[Category(Constants._category)]
+		[DefaultValue(false)]
+		public bool CslaNullableTypes
+		{
+			get 
+			{
+				bool result = false;
+				bool.TryParse(getValue(Constants._xnCslaNullableTypesNamespace), out result);
+				return result;
+			}
+			set { setValue(Constants._xnCslaNullableTypesNamespace, value.ToString()); }
+		}
+
+		[DisplayName("Csla Multiple Files Output")]
+		[Description("Generate classes in multiple output files.")]
+		[Category(Constants._category)]
+        [DefaultValue(CslaMultipleFilesOutput.AllClassesInSingleFile)]
+		public CslaMultipleFilesOutput CslaMultipleFiles
+		{
+			get
+			{
+                CslaMultipleFilesOutput result = CslaMultipleFilesOutput.AllClassesInSingleFile;
+                Enum.TryParse<CslaMultipleFilesOutput>(getValue(Constants._xnCslaMultipleFilesNamespace), out result);
+				return result;
+			}
+            set { setValue(Constants._xnCslaMultipleFilesNamespace, value.ToString()); }
+		}
+
+        [DisplayName("Csla Transaction Type")]
+        [Description("Transaction type for insert, update and delete operations in Data Portal.")]
+        [Category(Constants._category)]
+        [DefaultValue(CslaTransactionalTypes.TransactionScope)]
+        public CslaTransactionalTypes CslaTransactionType
+        {
+            get
+            {
+                CslaTransactionalTypes result = CslaTransactionalTypes.TransactionScope;
+                Enum.TryParse<CslaTransactionalTypes>(getValue(Constants._xnCslaTransactionType), out result);
+                return result;
+            }
+            set { setValue(Constants._xnCslaTransactionType, value.ToString()); }
+        }
+	}
+}
---
+++ Main/CslaExtension.Designer/Designer/CslaDesignerExtensionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+using System.ComponentModel.Composition;
+using Microsoft.Data.Entity.Design.Extensibility;
+
+namespace CslaExtension.Designer
+{
+	[PartCreationPolicy(CreationPolicy.Shared)]
+	[Export(typeof(IEntityDesignerExtendedProperty))]
+	[EntityDesignerExtendedProperty(EntityDesignerSelection.DesignerSurface)]
+	class CslaDesignerExtensionFactory : IEntityDesignerExtendedProperty
+	{
+		public object CreateProperty(XElement element, PropertyExtensionContext context)
+		{
+			return new CslaDesignerExtension(element, context);
+		}
+	}
+}
